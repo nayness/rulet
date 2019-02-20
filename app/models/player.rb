b40f5502ddfee605f3821ab8ca6ef895cbb6b1eb
nayness <nayness@gmail.com>
2019-02-20 08:57:54 -0300

    Refact gamble logic

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,13 +3,12 @@ class Player < ApplicationRecord
   has_many :rounds, through: :gambles
 
   def gamble
-    current = 0
-    return if cash <= 0
-    if cash <= 1000
-      current = cash
+    return 0 if cash <= 0
+    case cash
+    when cash<= 1000
+      cash
     else
-      current = cash * rand(8..15)/100.to_f
+      cash * rand(8..15)/100.to_f
     end
-    current
   end
 end
