cc00f632597003c8981e6a0f06afaadcc209789c
nayness <nayness@gmail.com>
2019-02-20 10:09:08 -0300

    Add random players scope to player

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,8 @@ class Player < ApplicationRecord
   has_many :gambles
   has_many :rounds, through: :gambles
 
+  scope :random_players, ->{ Player.offset(rand(Player.count) - 5).limit(5) }
+
   def gamble
     return 0 if cash <= 0
     case cash
