4a518e648b38db331336405c56eed4344833aacd
nayness <nayness@gmail.com>
2019-02-20 10:10:42 -0300

    Add round routes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#home'
+  root 'rounds#table'
+  get '/gamble', to: 'rounds#table'
+  post '/gamble', to: 'rounds#create'
 end
