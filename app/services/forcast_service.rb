cd350d259c8f7c11657617219bd9ae6a7e607353
nayness <nayness@gmail.com>
2019-02-22 01:12:00 -0300

    Add: forcast max temperatures

--- app/services/forcast_service.rb
+++ app/services/forcast_service.rb
@@ -4,4 +4,13 @@ class ForcastService
     configuration.api_key = Figaro.env.forcast_api_key
   end
 
+  def weekly_max_temperatures
+    forcast = ForecastIO.forecast(-33.447487, -70.673676, params: { units: 'si' })
+    weekly_wheater = forcast.daily.data
+    weekly_wheater.each_with_object([]) do |daily, max_temperatures|
+      if daily.temperatureMax >= 20
+        max_temperatures << daily.temperatureMax
+      end
+    end
+  end
 end
